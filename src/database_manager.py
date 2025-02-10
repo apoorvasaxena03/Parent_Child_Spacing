17aa0ae021c885aa469749d0c3f94ae7219bb27f
Apoorva Saxena <Apoorva.Saxena@sitio.com>
2025-02-10 15:27:34 -0600

    Added coded

--- src/database_manager.py
+++ src/database_manager.py
@@ -64,3 +64,28 @@ class SQLTableManager:
         if self.connection is not None:
             self.connection.close()
             self.connection = None
+
+
+class DatabricksOdbcConnector:
+    
+    def __init__(self):
+        self.dsn_name:str = "Databricks"
+        self.connection = None
+
+    def connect(self):
+        # Establish a connection
+        connection_string:str = f"DSN={self.dsn_name}"
+        self.connection = pyodbc.connect(connection_string, autocommit=True)
+
+    def execute_query(self, sql_query:str):
+        if self.connection is None:
+            raise Exception("Not connected to Databricks. Call connect() first.")
+        
+        # Execute the query and fetch the result into a Pandas DataFrame
+        result_df = pd.read_sql(sql_query, self.connection)
+        return result_df
+
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.close()
+            self.connection = None
